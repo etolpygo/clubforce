de5cdd8af3c0d75ebd51e8a3099bd9af34e80430
ETC <etolpygo@gmail.com>
2021-09-20 05:57:05 +0100

    remove RawGradesController; read json from internal file instead

--- src/controllers/GradesController.php
+++ src/controllers/GradesController.php
@@ -5,23 +5,34 @@ class GradesController {
 
     private $grades;
 
-    public function __construct()
-    {
-        $response = file_get_contents('http://example.com/path/to/api/call?param1=5');
+    // public function __construct()
+    // {
+    //     $response = file_get_contents('request.json');
+    //     $response = json_decode($response);
+    //     // populate grades
+    // }
+
+    #[Route("/grades/", methods: ["GET"])]
+    public function index() { 
+
+        $response = file_get_contents('request.json');
         $response = json_decode($response);
-        // populate grades
+
+        var_dump($response );
+
+        // $response['status_code_header'] = 'HTTP/1.1 200 OK';
+        // $response['body'] = json_encode($result);
+        // return $response;
     }
+
 
     #[Route("/grades/{name}", methods: ["GET"])]
     public function get($name) { 
         $response['status_code_header'] = 'HTTP/1.1 200 OK';
         $response['body'] = json_encode($result);
+        return $response;
      }
 
-    #[Route("/grades/", methods: ["GET"])]
-    public function index() { 
-        $response['status_code_header'] = 'HTTP/1.1 200 OK';
-        $response['body'] = json_encode($result);
-    }
+
 
 }
--- src/index.php
+++ src/index.php
@@ -1,23 +1,18 @@
 <?php
 
 require 'controllers/GradesController.php';
-require 'controllers/RawGradesController.php';
 
-header("Access-Control-Allow-Origin: http://localhost:8000/");
-header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: GET");
-header("Access-Control-Max-Age: 3600");
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+// header("Access-Control-Allow-Origin: http://localhost:8000/");
+// header("Content-Type: application/json; charset=UTF-8");
+// header("Access-Control-Allow-Methods: GET");
+// header("Access-Control-Max-Age: 3600");
+// header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $uri = explode('/', $uri);
 
-if ($uri[1] == 'raw_grades') {
-    $controller = new RawGradesController();
-    $response = $controller->index();
-}
-else if ($uri[1] == 'grades') {
+if ($uri[1] == 'grades') {
     $controller = new GradesController();
     if (isset($uri[2])) {
         $name = preg_replace( '/[\W]/', '', $uri[2]);
@@ -31,7 +26,7 @@ else {
     exit();
 }
 
-header($response['status_code_header']);
-if ($response['body']) {
-    echo $response['body'];
-}
+// header($response['status_code_header']);
+// if ($response['body']) {
+//     echo $response['body'];
+// }
