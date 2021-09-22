6443bee72d71083be7b971206001a0cfe23b0fa8
ETC <etolpygo@gmail.com>
2021-09-22 04:30:06 +0100

    catch some error cases

--- src/controllers/GradesController.php
+++ src/controllers/GradesController.php
@@ -9,18 +9,25 @@ class GradesController {
         $grades = array();
 
         $data = json_decode(file_get_contents('php://input'), true);
-        foreach($data as $record) {
-            $grade = new Grade($record['name'], (int)$record['grade']);
-            $grades[] = $grade;
+        if (json_last_error() === JSON_ERROR_NONE) {
+            foreach($data as $record) {
+                if (isset($record['name']) && isset($record['grade'])) {
+                    $grade = new Grade((string)$record['name'], (int)$record['grade']);
+                    $grades[] = $grade;
+                }
+                // otherwise, skip it
+            }
+            $result = array();
+            foreach($grades as $grade) {
+                $result[] = $grade->to_array();
+            }
+            $response['status_code_header'] = 'HTTP/1.1 200 OK';
+            $response['body'] = json_encode($result);
+        }
+        else { // error parsing json
+            $response['status_code_header'] = 'HTTP/1.1 400 Bad Request';
         }
 
-        $result = array();
-        foreach($grades as $grade) {
-            $result[] = $grade->to_array();
-        }
-
-        $response['status_code_header'] = 'HTTP/1.1 200 OK';
-        $response['body'] = json_encode($result);
         return $response;
     }
 
