8b66e56ae622f35f0599e677dbab3fb74595c184
ETC <etolpygo@gmail.com>
2021-09-22 02:55:29 +0100

    allow only POST request

--- src/index.php
+++ src/index.php
@@ -4,13 +4,20 @@ require 'controllers/GradesController.php';
 
 header("Access-Control-Allow-Origin: http://localhost:8000/");
 header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: GET");
+header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $uri = explode('/', $uri);
+
+$requestMethod = $_SERVER["REQUEST_METHOD"]; 
+if ($requestMethod !== 'POST') {
+    http_response_code(400);
+    echo "Only POST method is allowed";
+    exit();
+} 
 
 if ($uri[1] == 'grades') {
     
