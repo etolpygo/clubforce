39dfae0b7866467838a00d8a28a2d3d9c4d1dfce
ETC <etolpygo@gmail.com>
2021-09-22 02:35:58 +0100

    add test setup and another model test

--- src/tests/GradeTest.php
+++ src/tests/GradeTest.php
@@ -1,38 +1,61 @@
 <?php 
 declare(strict_types=1);
 
-require 'models/Grade.php';
-
 use PHPUnit\Framework\TestCase;
 
 final class GradeTest extends TestCase
-{
-    public function testGetRoundedGrade(): void
+{   
+    private $grade1, $grade2, $grade3, $grade4;
+
+    protected function setUp(): void
     {
-        $grade1 = new Grade("John", 53);
-        $grade2 = new Grade("Jane", 68);
-        $grade3 = new Grade("Emma", 32);
-        $grade4 = new Grade("Sophia", 39);
+        $this->grade1 = new Grade("John", 53);
+        $this->grade2 = new Grade("Jane", 68);
+        $this->grade3 = new Grade("Emma", 32);
+        $this->grade4 = new Grade("Sophia", 39);
+    }
+
+    /**
+     * @covers Grade::getRoundedGrade
+     */
+    public function testGetRoundedGrade(): void
+    {   
 
         $this->assertEquals(
             55,
-            $grade1->getRoundedGrade()
+            $this->grade1->getRoundedGrade()
         );
 
         $this->assertEquals(
             70,
-            $grade2->getRoundedGrade()
+            $this->grade2->getRoundedGrade()
         );
 
         $this->assertEquals(
             30,
-            $grade3->getRoundedGrade()
+            $this->grade3->getRoundedGrade()
         );
 
         $this->assertEquals(
             40,
-            $grade4->getRoundedGrade()
+            $this->grade4->getRoundedGrade()
         );
+
+    }
+
+    /**
+     * @covers Grade::pass
+     */
+    public function testPass(): void
+    {   
+
+        $this->assertTrue($this->grade1->pass());
+
+        $this->assertTrue($this->grade2->pass());
+
+        $this->assertFalse($this->grade3->pass());
+
+        $this->assertTrue($this->grade4->pass());
 
     }
 
